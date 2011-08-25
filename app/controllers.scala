3dd4338c644dd9faf8d05f8151ea48a46ed850d9
Soeren Roerden <soeren.roerden@patch.com>
2011-08-24 22:05:26 -0400

    Add browse endpoint

--- app/controllers.scala
+++ app/controllers.scala
@@ -5,25 +5,30 @@ import play.mvc._
 import dispatch.{Request, Http => DHttp, url, :/}
 
 object Application extends Controller {
-    
-    import views.Application._
+  import views.Application._
 
-    val http = new DHttp
-    
-    def index = {
-        html.index()
-    }
+  val veloHost = Play.configuration.get("velocigraphtor.host").toString
+  val veloPort = Play.configuration.get("velocigraphtor.port").toString.toInt
+  val http = new DHttp
 
-    def metrics(path: Option[String], start: Option[String], end: Option[String]) = {
-        val veloHost = Play.configuration.get("velocigraphtor.host").toString
-        val veloPort = Play.configuration.get("velocigraphtor.port").toString.toInt
-        (path, start, end) match {
-            case (Some(path), Some(start), Some(end)) => {
-              val req = :/(veloHost, veloPort) <<? Map("path" -> path, "start" -> start, "end" -> end)
-              http(req >- { x => Text(x) })
-              // Text(result)
-            }
-            case _ => Text("missing parameters, set path, start, end")
-        }
+  def index = {
+    html.index()
+  }
+
+  def browse(path: Option[String]) = {
+    val p = path.getOrElse(Text("missing parameters, please set path"))
+    val req = (:/(veloHost, veloPort) / browse) <<? Map("path" -> path)
+    http(req >- { x => Text(x) })
+  }
+
+  def metrics(path: Option[String], start: Option[String], end: Option[String]) = {
+    (path, start, end) match {
+      case (Some(path), Some(start), Some(end)) => {
+        val req = :/(veloHost, veloPort) <<? Map("path" -> path, "start" -> start, "end" -> end)
+        http(req >- { x => Text(x) })
+        // Text(result)
+      }
+      case _ => Text("missing parameters, set path, start, end")
     }
+  }
 }
